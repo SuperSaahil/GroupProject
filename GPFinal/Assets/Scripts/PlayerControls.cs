9bf9377c06591120e77a92355dccfbb6445a8a99
aswd2005 <128409186+aswd2005@users.noreply.github.com>
2023-05-18 10:09:29 -0700

    Animation
    Animation complet

--- GPFinal/Assets/Scripts/PlayerControls.cs
+++ GPFinal/Assets/Scripts/PlayerControls.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 9;
     public float MoveDirection;
-    public Rigidbody2D  rb;
+    public Rigidbody2D rb;
     public bool isGrounded = true;
     public int Damage;
     public bool RegPunch = false;
@@ -22,7 +22,7 @@ public class PlayerControls : MonoBehaviour
     public Transform shootPos;
     public GameObject bulletPrefab;
     public int playerHP = 6;
- 
+
 
 
 
@@ -33,8 +33,8 @@ public class PlayerControls : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         Arm = GameObject.FindGameObjectWithTag("PlayerArm").GetComponent<ArmCollider>();
-       
-        
+
+
     }
 
     // Update is called once per frame
@@ -45,122 +45,144 @@ public class PlayerControls : MonoBehaviour
         ReachATK();
         Jumping();
         Shooting();
-       
+        HorizontalAnimation();
+        WaveAnimation();
     }
     private void FixedUpdate()
     {
-       Move();
+        Move();
     }
 
 
     void PlayerInputSystem()
     {
         MoveDirection = Input.GetAxis("Horizontal");
-        
+        //  anim.SetTrigger("PlayerA");
+
+    }
+
+    void HorizontalAnimation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetTrigger("PlayerA");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetTrigger("PlayerA");
+        }
+    }
+    void WaveAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Runner.Wave");
+        }
     }
     void Jumping()
-    {
-        if(Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
-            rb.velocity = new Vector3(0, 6, 0);
-            isGrounded = false;
+            if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+            {
+                rb.velocity = new Vector3(0, 9, 0);
+                isGrounded = false;
+            }
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "ground" || collision.gameObject.tag == "platform")
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            isGrounded = true;
+            if (collision.gameObject.tag == "ground" || collision.gameObject.tag == "platform")
+            {
+                isGrounded = true;
+            }
+            else collision.gameObject.CompareTag("platform");
+            {
+                transform.parent = collision.transform;
+            }
         }
-        else collision.gameObject.CompareTag("platform");
+        private void OnCollisionExit2D(Collision2D collision)
         {
-            transform.parent = collision.transform;
+            transform.parent = null;
         }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        transform.parent = null;
-    }
 
-    void PlayerFlip()
-    {
-        facingRight = !facingRight;
-        transform.Rotate(0f, 180f, 0f);
-    }
-    private void Move()
-    {
-
-
-        //animate
-        if (MoveDirection > 0 && !facingRight)
+        void PlayerFlip()
         {
-            PlayerFlip();
+            facingRight = !facingRight;
+            transform.Rotate(0f, 180f, 0f);
         }
-        else if (MoveDirection < 0 && facingRight)
+        private void Move()
         {
-            PlayerFlip();
-        }
-        //Move
-        rb.velocity = new Vector2(MoveDirection * speed, rb.velocity.y);
-    }
 
 
-    void BasicPunch()
-    {
-        if(Input.GetKey(KeyCode.E) && canReg == true)
-        {
-            canReg = false;
-            isReg = true;
-            anim.SetTrigger("AttackStart");
-            StartCoroutine(punchCD()); 
+            //animate
+            if (MoveDirection > 0 && !facingRight)
+            {
+                PlayerFlip();
+            }
+            else if (MoveDirection < 0 && facingRight)
+            {
+                PlayerFlip();
+            }
+            //Move
+            rb.velocity = new Vector2(MoveDirection * speed, rb.velocity.y);
         }
-              
-    }
-    void ReachATK()
-    {
-        if (Input.GetKey(KeyCode.R) && canReach == true)
-        {
-            canReach = false;
-            isReached = true;
-            anim.SetTrigger("ReachAttack");
-            StartCoroutine(reachCD());  
-            
-           
-        }
-    }
 
-    void Shooting()
-    {
-        if(Input.GetKeyDown(KeyCode.Tab))
+
+        void BasicPunch()
         {
-            Instantiate(bulletPrefab, shootPos.position, transform.rotation);
+            if (Input.GetKey(KeyCode.E) && canReg == true)
+            {
+                canReg = false;
+                isReg = true;
+                anim.SetTrigger("AttackStart");
+                StartCoroutine(punchCD());
+            }
+
         }
-    }
+        void ReachATK()
+        {
+            if (Input.GetKey(KeyCode.R) && canReach == true)
+            {
+                canReach = false;
+                isReached = true;
+                anim.SetTrigger("ReachAttack");
+                StartCoroutine(reachCD());
+
+
+            }
+        }
+
+        void Shooting()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Instantiate(bulletPrefab, shootPos.position, transform.rotation);
+            }
+        }
+
+
+
+
+
+        IEnumerator punchCD()
+        {
+            yield return new WaitForSeconds(1f);
+            canReg = true;
+            isReg = false;
+
+        }
+        IEnumerator reachCD()
+        {
+            yield return new WaitForSeconds(3f);
+            canReach = true;
+            isReached = false;
+
+        }
+
+
+
+
+
+
+
     
-   
-
-
-   
-    IEnumerator punchCD()
-    {
-        yield return new WaitForSeconds(1f);
-        canReg = true;
-        isReg = false;
-
-    }
-    IEnumerator reachCD()
-    {
-        yield return new WaitForSeconds(3f);
-        canReach = true;
-        isReached = false;
-
-    }
-
-   
-
-
-
-
-
 }
